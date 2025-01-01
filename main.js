7e22e7928834a6c66f905d3fc7a519d48f239970
bluemoonweed <183841417+bluemoonweed@users.noreply.github.com>
2025-01-01 15:23:51 +0800

    logic for rounds

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
@@ -19,6 +22,26 @@ function getHumanChoice() {
       return getHumanChoice();
     }
   }
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie! Both chose ${humanChoice}`);
+  } else if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+  } else {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  }
+  console.log(`Scores: Human - ${humanScore}, Computer - ${computerScore}`);
+}
   
 console.log(getComputerChoice());
 console.log(getHumanChoice());
+
+playRound(humanScore, computerScore);
