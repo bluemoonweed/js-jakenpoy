5bcc7e2871503f126530bb2115b95a4f79dfe724
bluemoonweed <183841417+bluemoonweed@users.noreply.github.com>
2025-01-01 15:12:00 +0800

    logic for computer side

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+}
+
+console.log(getComputerChoice());
